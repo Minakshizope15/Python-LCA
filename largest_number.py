5d591f289481b0ccbf8d9e07be95aad31d1e15a7
Minakshizope15 <minakshizope15@gmail.com>
2026-08-22 13:51:09 +0530

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -1,25 +1,29 @@
 print("To find the largest number")
-a=float(input("Enter first number a:"))
-b=float(input("Enter second number b:"))
-c=float(input("enter third number c:"))
+a = float(input("Enter first number a: "))
+b = float(input("Enter second number b: "))
+c = float(input("Enter third number c: "))
 
-if a==b==c:
-   print("all are equal")
-elif a==b and a>c:
-   largest=a and b
-  else largest=c 
-elif b==c and b>a:
-   largest=b and c
-  else largest=a
-elif c==a and c>b:
-  largest=c and b 
- else largest=b
-if a>b and a>c:
-   largest=a
-elif b>c and b>a:
-   largest=b
+if a == b == c:
+    print("All are equal")
+
+elif a == b and a > c:
+    largest = a
+
+elif b == c and b > a:
+    largest = b
+
+elif c == a and c > b:
+    largest = c
+
+if a > b and a > c:
+    largest = a
+
+elif b > c and b > a:
+    largest = b
+
 else:
-   largest=c
-print("largest number is:", largest)
+    largest = c
+
+print("Largest number is:", largest)
 
   
