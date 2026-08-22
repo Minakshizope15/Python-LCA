657c8b34f259b52f663f7b5d003151cbfc124dab
Minakshizope15 <minakshizope15@gmail.com>
2026-08-22 13:25:12 +0530

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -7,18 +7,18 @@ if a==b==c:
    print("all are equal")
 elif a==b and a>c:
    largest=a and b
-   else largest=c 
+  else largest=c 
 elif b==c and b>a:
    largest=b and c
-   else largest=a
-elif c==a and c>b
+  else largest=a
+elif c==a and c>b:
   largest=c and b 
-  else largest=b
-if a>b and a>c
+ else largest=b
+if a>b and a>c:
    largest=a
-elif b>c and b>a
+elif b>c and b>a:
    largest=b
-elif c>a and c>b
+else:
    largest=c
 print("largest number is:", largest)
 
