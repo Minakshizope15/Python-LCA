fc35957250bdfb145dbce20407aa2296038112b9
Minakshizope15 <minakshizope15@gmail.com>
2026-09-10 12:27:12 +0530

    Add numpy array addition example
    addition of two matrices (numpy)

---
+++ numpy_addition_of_two_arrays.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr1= np.array([[1,2] , [3,4]]) 
+arr2= np.array([[5,6] , [7,8]])
+sum=arr1+arr2 
+print("array 1=",arr1) 
+print("array 2=",arr2)
+print("addition of two arrays=",sum)
